c8923d39640c35182a4c60e0e3a3d9bd4b0e6673
JET <jet@t520>
2016-08-13 22:37:47 -0500

    first commit

---
+++ src/isort.go
@@ -0,0 +1,116 @@
+/* isort.go
+
+   These are toy implementations of mergesort and quicksort that operate only
+   over int slices. They can substantially outperform sort.Ints(), but certainly
+   aren't meant as replacements for the generalized sort package. Several
+   pivoting strategies are provided; clients may also implement their own. */
+
+package isort
+
+import (
+	"math/rand"
+)
+
+func msort(a []int, b []int) {
+	mid := len(a) / 2
+	if mid >= 2 {
+		msort(a[:mid], b)
+	}
+	if len(a)-mid >= 2 {
+		msort(a[mid:], b)
+	}
+
+	copy(b, a)
+
+	for i, j, k := 0, mid, 0; k < len(a); k++ {
+		if i == mid {
+			a[k] = b[j]
+			j++
+		} else if j == len(a) || b[i] < b[j] {
+			a[k] = b[i]
+			i++
+		} else {
+			a[k] = b[j]
+			j++
+		}
+	}
+}
+
+/* Mergesort */
+func Msort(a []int) {
+	b := make([]int, len(a))
+	msort(a, b)
+}
+
+/* Chooses a pivot and returns its (index, value). */
+type Pivoter func([]int) (int, int)
+
+func Random(a []int) (int, int) {
+	i := rand.Intn(len(a))
+	return i, a[i]
+}
+
+func Middle(a []int) (int, int) {
+	i := len(a) / 2
+	return i, a[i]
+}
+
+/* Selects the median of the first, middle and last elements of the slice. */
+func Median3(a []int) (int, int) {
+	i, j := len(a)/2, len(a)-1
+	lo, mid, hi := a[0], a[i], a[j]
+	switch {
+	case mid <= lo && lo <= hi:
+		return 0, lo
+	case lo <= mid && mid <= hi:
+		return i, mid
+	default:
+		return j, hi
+	}
+}
+
+/* Selects the median among the medians-of-three of the thirds of the slice. */
+func Ninther(a []int) (int, int) {
+	if len(a) < 3 {
+		return Median3(a)
+	}
+
+	midStart, midEnd := len(a)/3, 2*len(a)/3
+	var i, m [3]int
+	i[0], m[0] = Median3(a[:midStart])
+	i[1], m[1] = Median3(a[midStart:midEnd])
+	i[2], m[2] = Median3(a[midEnd:])
+	i[1] += midStart
+	i[2] += midEnd
+	j, p := Median3(m[:])
+	return i[j], p
+}
+
+/* Quicksort */
+func Qsort(a []int, piv Pivoter) {
+	if len(a) < 2 {
+		return
+	}
+
+	k, p := piv(a)
+	r := len(a) - 1
+	a[k], a[r] = a[r], a[k]
+
+	i := 0
+	for j, x := range a[:r] {
+		if x <= p {
+			a[i], a[j] = x, a[i]
+			i++
+		}
+	}
+
+	a[i], a[r] = a[r], a[i]
+
+	Qsort(a[:i], piv)
+	Qsort(a[i+1:], piv)
+}
+
+/* A sensible default. */
+func Sort(a []int) {
+	Qsort(a, Median3)
+}
---
+++ src/isort_test.go
@@ -0,0 +1,143 @@
+package isort
+
+import (
+	"math/rand"
+	"sort"
+	"testing"
+	"time"
+)
+
+const (
+	maxRandBits = 16
+	maxRandLen  = 1<<maxRandBits - 1
+	maxRandElem = 99999
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomize(a []int) {
+	for i := range a {
+		a[i] = rand.Int()
+	}
+}
+
+/* This is a poor man's RNG of uniformly-distributed magnitudes. It should
+   produce a much smaller mean than the 2**30 of a uniform 32-bit generator. */
+func randMagOdd(nbits int) int {
+	if nbits == 0 {
+		return 0
+	}
+	if nbits > 32 {
+		panic("requested random magnitude exceeds 32 bits")
+	}
+
+	mag := rand.Intn(nbits)
+	return 1<<uint(mag) + randMagOdd(mag)
+}
+
+func randMag() int {
+	n := randMagOdd(maxRandBits)
+	return n ^ rand.Intn(2) // randomize parity
+}
+
+func benchSort(b *testing.B, sortf func([]int)) {
+	var arr [maxRandLen]int
+
+	b.ResetTimer()
+	// TODO: this len randomization is somewhat stupid
+	for i := 0; i < b.N; i++ {
+		a := arr[:rand.Intn(len(arr))]
+		randomize(a)
+		sortf(a)
+	}
+}
+
+func BenchmarkMsort(b *testing.B) {
+	buf := make([]int, maxRandLen) // save on reallocation costs
+	wrapper := func(a []int) {
+		msort(a, buf)
+	}
+	benchSort(b, wrapper)
+}
+
+func BenchmarkQsortRandom(b *testing.B) {
+	wrapper := func(a []int) {
+		Qsort(a, Random)
+	}
+	benchSort(b, wrapper)
+}
+
+func BenchmarkQsortMiddle(b *testing.B) {
+	wrapper := func(a []int) {
+		Qsort(a, Middle)
+	}
+	benchSort(b, wrapper)
+}
+
+func BenchmarkQsortMedian3(b *testing.B) {
+	wrapper := func(a []int) {
+		Qsort(a, Median3)
+	}
+	benchSort(b, wrapper)
+}
+
+func BenchmarkQsortNinther(b *testing.B) {
+	wrapper := func(a []int) {
+		Qsort(a, Ninther)
+	}
+	benchSort(b, wrapper)
+}
+
+/* Benchmarks sort.Ints() */
+func BenchmarkInts(b *testing.B) {
+	benchSort(b, sort.Ints)
+}
+
+func TestMQsort(t *testing.T) {
+	randUnsorted := make([]int, randMag())
+	for i := range randUnsorted {
+		randUnsorted[i] = rand.Intn(maxRandElem + 1)
+	}
+
+	tests := [][]int{
+		[]int{},
+		[]int{0},
+		[]int{0, 0},
+		[]int{0, 0, 0},
+		[]int{0, 1},
+		[]int{1, 0},
+		[]int{0, 0, 1},
+		[]int{0, 1, 0},
+		[]int{1, 0, 0},
+		[]int{0, 1, 1},
+		[]int{1, 0, 1},
+		[]int{1, 1, 0},
+		[]int{0, 1, 2},
+		[]int{0, 2, 1},
+		[]int{1, 0, 2},
+		[]int{1, 2, 0},
+		[]int{2, 0, 1},
+		[]int{2, 1, 0},
+		randUnsorted,
+	}
+	pivs := []Pivoter{Random, Middle, Median3, Ninther}
+
+	for i, test := range tests {
+		a := make([]int, len(test))
+		copy(a, test)
+		Msort(a)
+		if !sort.IntsAreSorted(a) {
+			t.Fatalf("Msort of test %d failed: %v => %v\n", i, test, a)
+		}
+
+		for j, piv := range pivs {
+			copy(a, test)
+			Qsort(a, piv)
+			if !sort.IntsAreSorted(a) {
+				t.Errorf("Qsort with pivoter %d of test %d failed: %v => %v\n", j, i, test, a)
+			}
+		}
+	}
+}
